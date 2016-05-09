0668186b3c28f19be572a7437b9f33ad1df13613
mgng <mgngmgng@gmail.com>
2016-05-09 11:16:01 +0900

    jQuery CDN を変更

--- _apps/template/Scrv/_parts/meta_common.tpl.php
+++ _apps/template/Scrv/_parts/meta_common.tpl.php
@@ -7,7 +7,10 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width" />
 <meta name="format-detection" content="telephone=no" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script
+	src="https://code.jquery.com/jquery-2.2.3.min.js"
+	integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="
+	crossorigin="anonymous"></script>
 <link href="<?= h($base_path) ?>favicon.ico" type="image/x-icon" rel="icon" />
 <link href="<?= h($base_path) ?>favicon.ico" type="image/x-icon" rel="shortcut icon" />
 <link rel="stylesheet" href="<?= h($base_path) ?>css/scrvm.css?v8" />
