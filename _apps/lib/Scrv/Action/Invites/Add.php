b599652222709536a11b4be2e21c8b8ad6519f78
mgng <mgngmgng@gmail.com>
2016-05-10 13:51:26 +0900

    Invites.Add の際にログインセッションが切れてしまっていたので修正

--- _apps/lib/Scrv/Action/Invites/Add.php
+++ _apps/lib/Scrv/Action/Invites/Add.php
@@ -74,10 +74,13 @@ class Add extends Base
 		$this->_Session->clear(Scrv\SessionKeys::INVITATIONS_DATA);
 
 		// ログイン済みとして処理
+		$timeout = 30 * 60;
 		$this->_Session->init();
 		$this->_Session->regenerate();
 		$this->_Session->set(Scrv\SessionKeys::IS_LOGIN, true);
 		$this->_Session->set(Scrv\SessionKeys::LOGIN_USER_DATA, $add_result["data"]);
+		$this->_Session->set(Scrv\SessionKeys::LOGIN_TIMEOUT, $timeout);
+		$this->_Session->set(Scrv\SessionKeys::LOGIN_EXPIRES, $timeout + $this->_nowTimestamp);
 
 		//Server::redirect($this->_BasePath . "Users/View?id=" . $add_result["data"]["id"]);
 		Server::redirect($this->_BasePath . "About");
