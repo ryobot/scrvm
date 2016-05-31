5a43f6f159ef88e5c8cb3cb0b10bf00575fc7dc9
mgng <mgngmgng@gmail.com>
2016-05-31 17:19:08 +0900

    アルバムタイトルの "[...]"を除去してからクエリ実行

--- _apps/template/Scrv/Albums/View.tpl.php
+++ _apps/template/Scrv/Albums/View.tpl.php
@@ -187,13 +187,16 @@ $view_year = isset($album["year"]) && $album["year"] !== "" ? $album["year"] : "
 <script>
 ;$(function(){
 
-	// 検索文字列の title に [Bonus Track]って入ってると正しく検索できないから除去しておく
-	$("#id_term").val(
-		$("#id_term").val().replace(/\s*\[\s*Bonus\s*Track\s*\]\s*$/i, "")
-	);
-
 	var ARTIST = $("#id_term").attr("data-artist");
 	var TITLE = $("#id_term").attr("data-title");
+
+	// title に []があると検索がうまくいかないから削除
+	var _tmp_title = TITLE.replace(/\[.+?\]/, "");
+	if ( _tmp_title !== "" ) {
+		$("#id_term").val( ARTIST + " " + _tmp_title );
+	} else {
+		$("#id_term").val( ARTIST + " " + TITLE.replace(/[\[\]]/g, "") );
+	}
 
 	// tag_data
 	$(".tags > a").each(function(){
