2e7392970a26c95b4a679d0d89f61ad87a236bdb
mgng <mgngmgng@gmail.com>
2016-06-06 11:13:36 +0900

    album.edit の画像アップロード前にサムネイル表示

--- _apps/template/Scrv/Albums/Edit.tpl.php
+++ _apps/template/Scrv/Albums/Edit.tpl.php
@@ -30,48 +30,61 @@
 	<form id="id_Albums_EditRun" enctype="multipart/form-data" action="javascript:;" method="POST" autocomplete="off" class="form_info">
 		<input type="hidden" name="id" value="<?= h($id) ?>" />
 		<input type="hidden" name="token" value="<?= h($token) ?>" />
-		<table class="w100per" id="id_album_basic_info">
-			<tr>
-				<td class="w50px">cover</td>
-				<td>
-					<img src="<?= !isset($album["img_file"]) || $album["img_file"] === "" ? h("{$base_path}img/no_image.png") : h("{$base_path}files/covers/{$album["img_file"]}") ?>" alt="<?= h("{$album["artist"]} / {$album["title"]}") ?>" class="album_view_cover" />
-					<p class="actions"><input name="file" type="file" accept="image/*" /></p>
-				</td>
-			</tr>
-			<tr>
-				<td>artist</td>
-				<td><input type="text" name="artist" id="id_artist" value="<?= h($album["artist"]) ?>" required="require" /></td>
-			</tr>
-			<tr>
-				<td>title</td>
-				<td><input type="text" name="title" id="id_title" value="<?= h($album["title"]) ?>" required="require" /></td>
-			</tr>
-			<tr>
-				<td>year</td>
-				<td><input type="text" name="year" id="id_year" value="<?= h($album["year"]) ?>" required="require" /></td>
-			</tr>
-		</table>
+		<div class="displaytable w100per mgb5px">
+			<div class="displaytablecell w50px">cover</div>
+			<div class="displaytablecell drop_zone" id="id_drop_zone">
+				<div class="displaytablecell w120px">
+					<img
+						src="<?= !isset($album["img_file"]) || $album["img_file"] === "" ? h("{$base_path}img/no_image.png") : h("{$base_path}files/covers/{$album["img_file"]}") ?>"
+						alt="<?= h("{$album["artist"]} / {$album["title"]}") ?>"
+						class="album_view_cover"
+						id="id_album_view_cover"
+						data-src_org="<?= !isset($album["img_file"]) || $album["img_file"] === "" ? h("{$base_path}img/no_image.png") : h("{$base_path}files/covers/{$album["img_file"]}") ?>"
+					/>
+				</div>
+				<div class="displaytablecell pdl10px">
+					<div>
+						<img src="<?= h($base_path) ?>img/image.svg" class="img16x16" alt="画像を変更する" />
+						画像を変更する
+					</div>
+				</div>
+			</div>
+			<p class="actions displaynone"><input name="file" type="file" id="id_file" accept="image/*" /></p>
+		</div>
+		<div class="displaytable w100per">
+			<div class="displaytablecell w50px">artist</div>
+			<div class="displaytablecell">
+				<input type="text" name="artist" id="id_artist" value="<?= h($album["artist"]) ?>" required="require" />
+			</div>
+		</div>
+		<div class="displaytable w100per">
+			<div class="displaytablecell w50px">title</div>
+			<div class="displaytablecell">
+				<input type="text" name="title" id="id_title" value="<?= h($album["title"]) ?>" required="require" />
+			</div>
+		</div>
+		<div class="displaytable w100per">
+			<div class="displaytablecell w50px">year</div>
+			<div class="displaytablecell">
+				<input type="text" name="year" id="id_year" value="<?= h($album["year"]) ?>" required="require" />
+			</div>
+		</div>
 		<p> </p>
-		<table class="w100per" id="id_tracks">
-			<tr>
-				<td class="w50px"></td>
-				<td></td>
-			</tr>
 <?php foreach($tracks as $track): ?>
-			<tr>
-				<td>tr.<?= h($track["track_num"]) ?></td>
-				<td><input
+		<div class="displaytable w100per">
+			<div class="displaytablecell w50px">tr.<?= h($track["track_num"]) ?></div>
+			<div class="displaytablecell">
+				<input
 					type="text"
 					name="tracks[]"
 					id="id_track_num_<?= h($track["track_num"]) ?>"
 					value="<?= h($track["track_title"]) ?>"
 					required="require"
-				/></td>
-			</tr>
+				/>
+			</div>
+		</div>
 <?php endforeach; ?>
-		</table>
-		<div id="id_Albums_SearchImage_result"></div>
-		<p class="actions tacenter"><input type="submit" value=" save " id="id_save" /></p>
+		<p class="actions tacenter"><input type="submit" value=" 保存する " id="id_save" /></p>
 	</form>
 
 </div>
@@ -80,8 +93,50 @@
 </div>
 
 <script>
+
 ;$(function(){
-	var post_url = "<?= h($base_path) ?>Albums/EditRun";
+
+	// drop_zoneのみdrag drop可能に
+	$("#id_drop_zone").on( "click.js", function(){
+		$("#id_file").trigger("click");
+	});
+	// クリックアップロード時は onChange イベントで処理
+	$("#id_file").on("change.js", function(){
+		var files = $(this).prop( "files" );
+		if ( files.length !== 1 ) {
+			return false;
+		}
+		var file = files[0];
+		var data = {
+			name : file.name,
+			size : file.size,
+			type : file.type
+		};
+		if ( ! checkFileFormat( data.type, data.size ) ) {
+			return false;
+		}
+		var fileReader = new FileReader();
+		fileReader.readAsDataURL( file );
+		fileReader.onload = function( event ) {
+			displayImage( this.result );
+			$("#id_drop_zone").removeClass().addClass("drop_zone_dragged");
+		};
+		fileReader.onloadend = function( event ){
+		};
+	});
+
+	var checkFileFormat = function( type, size ) {
+		if ( ! type.match( /^image\/(gif|png|jpeg)$/i ) ) {
+			alert( "画像形式が不正です。\n(allowed jpeg/png/gif)" );
+			return false;
+		}
+		return true;
+	};
+	var displayImage = function(base64){
+		$("#id_album_view_cover").attr({src:base64});
+	};
+
+	var post_url = BASE_PATH + "Albums/EditRun";
 	$("#id_save").on("click.js", function(){
 		if ( confirm("are you sure ?") ) {
 			$("#id_Albums_EditRun").attr({action:post_url}).submit();
