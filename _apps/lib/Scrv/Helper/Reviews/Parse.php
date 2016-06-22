a05f49a056092d72c901136e8071c52d75ddc30d
mgng <mgngmgng@gmail.com>
2016-06-22 16:31:00 +0900

    ハッシュタグ抽出正規表現変更

--- _apps/lib/Scrv/Helper/Reviews/Parse.php
+++ _apps/lib/Scrv/Helper/Reviews/Parse.php
@@ -16,7 +16,7 @@ class Parse
 	 * ハッシュタグ抽出用正規表現
 	 * @var string
 	 */
-	private $_reg_hashtags = "/\s#([^\W]+)/u";
+	private $_reg_hashtags = "/(^|\s)#([^\W]+)/u";
 
 	/**
 	 * body 内のハッシュタグを抜き出して配列で返す。
@@ -26,13 +26,13 @@ class Parse
 	public function hashTags($body)
 	{
 		preg_match_all($this->_reg_hashtags, $body, $match);
-		return $match[1];
+		return $match[2];
 	}
 
 	public function replaceHashTagsToLink($body, $base_path)
 	{
 		return preg_replace_callback($this->_reg_hashtags, function($match) use ($base_path){
-			return ' <a href="'.h($base_path).'Reviews/Index/hash/'.urlencode($match[1]).'">#'.h($match[1]).'</a>';
+			return ' <a href="'.h($base_path).'Reviews/Index/hash/'.urlencode($match[2]).'">#'.h($match[2]).'</a>';
 		}, $body);
 	}
 
